96bfd3e4df11b40709e2b7013959b20037325d02
rohitazad <ram9953878727@gmail.com>
2024-03-12 19:24:00 +0530

    seo and title dynamic redirect

--- src/app/blog/[...id]/page.tsx
+++ src/app/blog/[...id]/page.tsx
@@ -2,26 +2,47 @@
 
 import styles from "./styles.module.scss";
 import Link from "next/link";
+import { redirect } from 'next/navigation'
+
+
+const fetchPostDetails = async (blogId:any)=>{
+    const APIURL = `https://dummyjson.com/post/${blogId}`
+    const data = await fetch(APIURL);
+    const resData = data.json();
+    return resData;
+}
+
+export async function generateMetadata(props:any) {
+    const {params, searchParams} = props;
+    const blogId = params.id;
+    const getID = blogId.find((element:any) => element.startsWith("id-")).split("id-")[1];
+    const postData = await fetchPostDetails(getID);
+    return {
+        title: postData?.title,
+        desc: postData?.desc,
+        keywords: postData?.keyword,
+      }
+}
 
 const BlogDetails = async (props:any)=>{
     const {params, searchParams} = props;
     const blogId = params.id;
     const getID = blogId.find((element:any) => element.startsWith("id-")).split("id-")[1];
-
-    const fetchPostDetails = async ()=>{
-        const APIURL = `https://dummyjson.com/post/${getID}`
-        const data = await fetch(APIURL);
-        const resData = data.json();
-        return resData;
-    }
-    const postDetailsData = await fetchPostDetails();
+    const postData = await fetchPostDetails(getID);
+    const seoName = postData?.title?.trim().toLowerCase().replace(/\s+/g, "-");
+    const getPath = `id-${getID}/${seoName}`;
+    const urlPathName = params.id.join("/");
+    const redirectURL = `/blog/${getPath}`;
+    if (getPath !== urlPathName) {
+        redirect(redirectURL);
+      }
     return (
         <div className={`wraper ${styles.details}}`}>
-            <h1>{postDetailsData.title}</h1>
-            <p>{postDetailsData.body}</p>
+            <h1>{postData.title}</h1>
+            <p>{postData.body}</p>
             <div className={styles.tags}>
                 {
-                   postDetailsData.tags &&  postDetailsData.tags.length > 0 ? postDetailsData.tags.map(((tag:any, index:number)=>{
+                   postData.tags &&  postData.tags.length > 0 ? postData.tags.map(((tag:any, index:number)=>{
                     return <span key={`-${index}-${tag}`}>
                         {
                             tag
